5765044cbe7a3085453ef494fe6782c420e7c139
MCuetoV <96836482+MCuetoV@users.noreply.github.com>
2023-10-09 17:39:11 -0500

    Update page.js

--- src/app/pantalla_perfil_estudiante/page.js
+++ src/app/pantalla_perfil_estudiante/page.js
@@ -82,7 +82,7 @@ const pantallaPerfil = () =>{
                 {leftBarContent}
                 <div className={styles.contenido}>
                     <div className={styles.containerTitulo}>
-                        <h1 className={styles.titulo}>Mi Perfil gaaa</h1>
+                        <h1 className={styles.titulo}>Mi Perfil</h1>
                         
                     </div>
                     <div className={styles.linea}></div>
@@ -90,6 +90,7 @@ const pantallaPerfil = () =>{
                     <ButtonGroup className="mb-2">
                         <Button onClick={swapContenidoDU}>Datos Personales</Button>
                         <Button onClick={swapContenidoU}>Cuenta</Button>
+                        <Button onClick={swapContenidoP}>Preferencias</Button>
                     </ButtonGroup>                
                     </div>
                     <br></br>
