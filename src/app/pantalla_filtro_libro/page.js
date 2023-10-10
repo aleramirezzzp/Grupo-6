0228945e2eddda574120ed88cc10a1e9fb4ff8c2
MCuetoV <96836482+MCuetoV@users.noreply.github.com>
2023-10-09 21:13:19 -0500

    Update page.js filtro_libro con barra lateral

--- src/app/pantalla_filtro_libro/page.js
+++ src/app/pantalla_filtro_libro/page.js
@@ -2,13 +2,12 @@
 import Link from 'next/link'
 import styles from './page.module.css'
 import TopBar from '../../components/TopBar/TopBar.jsx'
-import LeftBar from '../../components/LeftBar/LeftBar.jsx'
+import LeftBar from '../../components/LeftBar/LeftBar.jsx' // Import LeftBar
 import React, { useState } from "react"
 import Button from "react-bootstrap/Button"
 import Input from '@/components/Input/Input'
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { useRouter } from 'next/navigation'
-
 
 const pantallaPerfil = () => {
 
@@ -34,7 +33,7 @@ const pantallaPerfil = () => {
     let leftBarContent
 
     if (topBarIsVisible) {
-        leftBarContent = <LeftBar />
+        leftBarContent = <LeftBar /> // Include LeftBar component
     }
 
     const [searchQuery, setSearchQuery] = useState("");
@@ -44,91 +43,93 @@ const pantallaPerfil = () => {
     const handleFilterChange = (event) => {
         const { value, checked } = event.target;
         if (checked) {
-          setSelectedFilters([...selectedFilters, value]);
+            setSelectedFilters([...selectedFilters, value]);
         } else {
-          setSelectedFilters(selectedFilters.filter((filter) => filter !== value));
+            setSelectedFilters(selectedFilters.filter((filter) => filter !== value));
         }
-      };
+    };
 
-      const handleClear = () => {
+    const handleClear = () => {
         setSearchQuery("");
         setBookType("");
         setSelectedFilters([]);
-      };
+    };
 
-      return (
+    return (
         <div className={styles.container}>
-          <div className={styles.main}>
-            <div className={styles.contenido}>
-              <div className={styles.containerTitulo}>
-                <h1 className={styles.titulo}>Búsqueda</h1>
-              </div>
-              <div className={styles.linea}></div>
-              <div className={styles.buscar}>
-                <input
-                  type="text"
-                  placeholder="Ingresa la palabra clave"
-                  className={styles.inputBusqueda}
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <input
-                  type="text"
-                  placeholder="Ingresa el tipo de libro"
-                  className={styles.inputBusqueda}
-                  value={bookType}
-                  onChange={(e) => setBookType(e.target.value)}
-                />
+            <TopBar onButtonClick={swapTopBar} />
+            <div className={styles.main}>
+                {leftBarContent} {/* Render the LeftBar component */}
+                <div className={styles.contenido}>
+                    <div className={styles.containerTitulo}>
+                        <h1 className={styles.titulo}>Búsqueda</h1>
+                    </div>
+                    <div className={styles.linea}></div>
+                    <div className={styles.buscar}>
+                        <input
+                            type="text"
+                            placeholder="Ingresa la palabra clave"
+                            className={styles.inputBusqueda}
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Ingresa el tipo de libro"
+                            className={styles.inputBusqueda}
+                            value={bookType}
+                            onChange={(e) => setBookType(e.target.value)}
+                        />
 
-                <div className={styles.filters}>
-                <p><h4>Incluir busqueda en:</h4> </p>
-                  <label>
-                    <input
-                      type="checkbox"
-                      value="Titulo"
-                      checked={selectedFilters.includes("Titulo")}
-                      onChange={handleFilterChange}
-                    />
-                    Título
-                  </label>
-                  <label>
-                    <input
-                      type="checkbox"
-                      value="Autor"
-                      checked={selectedFilters.includes("Autor")}
-                      onChange={handleFilterChange}
-                    />
-                    Autor
-                  </label>
-                  <label>
-                    <input
-                      type="checkbox"
-                      value="Serie"
-                      checked={selectedFilters.includes("Serie")}
-                      onChange={handleFilterChange}
-                    />
-                    Serie
-                  </label>
-                  <label>
-                    <input
-                      type="checkbox"
-                      value="ISBN"
-                      checked={selectedFilters.includes("ISBN")}
-                      onChange={handleFilterChange}
-                    />
-                    ISBN
-                  </label>
+                        <div className={styles.filters}>
+                            <p><h4>Incluir búsqueda en:</h4> </p>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    value="Titulo"
+                                    checked={selectedFilters.includes("Titulo")}
+                                    onChange={handleFilterChange}
+                                />
+                                Título
+                            </label>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    value="Autor"
+                                    checked={selectedFilters.includes("Autor")}
+                                    onChange={handleFilterChange}
+                                />
+                                Autor
+                            </label>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    value="Serie"
+                                    checked={selectedFilters.includes("Serie")}
+                                    onChange={handleFilterChange}
+                                />
+                                Serie
+                            </label>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    value="ISBN"
+                                    checked={selectedFilters.includes("ISBN")}
+                                    onChange={handleFilterChange}
+                                />
+                                ISBN
+                            </label>
+                        </div>
+                    </div>
+                    <div className={styles.buscar}>
+                        <button className={styles.boton} onClick={handleClear}>Limpiar</button>
+                        <button className={styles.boton}>Buscar</button>
+                    </div>
                 </div>
-              </div>
-              <div className={styles.buscar}>
-              <button className={styles.boton} onClick={handleClear}>Limpiar</button>
-                <button className={styles.boton}>Buscar</button>
-              </div>
             </div>
-          </div>
         </div>
-      );
-
+    );
 }
 
 export default pantallaPerfil;
+
